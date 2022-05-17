9593fa9e2fa6645b94eabd4dc10c6da20744d3c2
mandy2407 <amandacamila2407@gmail.com>
2022-05-16 20:35:37 -0400

    Atualizando dia 16/05.

--- servidor-paraolar/server.js
+++ servidor-paraolar/server.js
@@ -1,3 +1,22 @@
+const express = require("express")
+const app = express()
+
+app.get ("/Netmandy", (request, response)=>{
+    response.status(200).json(["Seja bem vindo à maior plataforma de séries e filmes do Brasil.Tudo certo!"])
+})
+
+app.get("/filmes", (request, response)=> {
+    response.status(200).json(["Aqui estão os filmes disponíveis, se divirta!"])
+})
+
+app.get("/series", (request, response)=> {
+    response.status(200).json(["Aqui estão os séries disponíveis, se divirta!"])
+})
+
+app.listen (8049, ()=>{
+console.log("Servidor está rodando na porta 8049!")
+})
+
 
   
   
